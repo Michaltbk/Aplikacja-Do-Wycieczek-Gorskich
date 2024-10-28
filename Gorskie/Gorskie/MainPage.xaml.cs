f9aa6003732de24d3bca6da8038beee354f496f1
Michaltbk <100926282+Michaltbk@users.noreply.github.com>
2024-10-28 22:05:57 +0100

    Dodanie NUGET Firebase i zmergowanie FireBas z Michaltbk

--- Gorskie/Gorskie/MainPage.xaml.cs
+++ Gorskie/Gorskie/MainPage.xaml.cs
@@ -1,17 +1,23 @@
-﻿using System;
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xamarin.Essentials;
 using Xamarin.Forms;
-using Xamarin.Forms.PlatformConfiguration;
-using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
 
 namespace Gorskie
 {
     public partial class MainPage : ContentPage
     {
+        FirebaseClient firebaseClient = new Firebase.Database.FirebaseClient("https://gorskie-610c5-default-rtdb.europe-west1.firebasedatabase.app/");
         public MainPage()
         {
             InitializeComponent();
@@ -19,21 +25,52 @@ namespace Gorskie
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            string imie = usernameEntry.Text;
-            string haslo = passwordEntry.Text;
 
-            if (imie == "admin" && haslo == "123")
+            if (Connectivity.NetworkAccess != NetworkAccess.Internet)
             {
-                await Navigation.PushAsync(new Glowna());
-            }
-            else
-            {
-                messageLabel.Text = "Podano zle dane";
+                messageLabel.Text = "Brak połączenia!";
+                return;
             }
 
+            string name = usernameEntry.Text;
+            string password = passwordEntry.Text;
 
+            var existingRecords = await firebaseClient
+                .Child("Records")
+                .OnceAsync<MyDatabaseRecord>();
+
+            foreach (var variableName in existingRecords)
+            {
+
+                if (variableName.Object.MyName == name && variableName.Object.MyPassword == password)
+                {
+                    await Navigation.PushAsync(new MapPage());
+                    return;
+                }
+            }
+            messageLabel.Text = "Złe hasło lub brak użytkownika";
         }
 
 
+        private async void Button_Register(object sender, EventArgs e)
+        {
+            if (Connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                messageLabel.Text = "Brak połączenia!";
+                return;
+            }
+
+            messageLabel.Text = "Zarejestrowano";
+
+            var record = new MyDatabaseRecord
+            {
+                MyName = usernameEntry.Text,
+                MyPassword = passwordEntry.Text
+            };
+
+            await firebaseClient.Child("Records").PostAsync(record);
+
+        }
     }
+
 }
